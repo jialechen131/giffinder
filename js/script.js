15a74e9a5a1f834ba5ea46b77492ef1ce3e0bcff
Jia Le Chen <jialechen321@gmail.com>
2018-04-11 22:26:07 +0000

    need to work on the vel

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 
 $(document).ready(function() {
     $.ajax({
-        url: "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC&limit=10",
+        url: "https://api.giphy.com/v1/stickers/search?q=cat&api_key=dc6zaTOxFJmzC&limit=1",
         method: "GET",
         success: function(response) {
             // Below log the response object to the console
@@ -20,20 +20,31 @@ $(document).ready(function() {
             //Log the url from the orignial gif to the console  
 
             console.log(response.data[0].images.original_still.url);
-            var img_url = response.data[8].images.original.url;
+            var img_url = response.data[0].images.original.url;
 
             $('body').append('<img src=' + img_url + '>');
 
         },
     });
+$("#search").click(function(){
+var searchterm = $("input").val();
+console.log(searchterm)
+var url= "https://api.giphy.com/v1/stickers/search?q="+searchterm+"&api_key=dc6zaTOxFJmzC&limit=1"
+console.log(url)
+    
+});
 
-function find() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC&limit=10", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send();
-    var response = JSON.parse(xhttp.responseText);
-}
+//function find() {
+  //  var xhttp = new XMLHttpRequest();
+//    xhttp.open("POST", "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC&limit=10", true);
+ //   xhttp.setRequestHeader("Content-type", "application/json");
+  //  xhttp.send();
+//    var response = JSON.parse(xhttp.responseText);
+//}
+
+
+
+
 
 
 });
