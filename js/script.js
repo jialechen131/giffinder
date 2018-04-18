ae31979e91a22ec0b248df366eabd66df67cac6b
Jia Le Chen <jialechen321@gmail.com>
2018-04-18 02:42:19 +0000

    add the code

--- js/script.js
+++ js/script.js
@@ -1,50 +1,38 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-
+ function getGiphy(searchterm){
 $(document).ready(function() {
     $.ajax({
-        url: "https://api.giphy.com/v1/stickers/search?q=cat&api_key=dc6zaTOxFJmzC&limit=1",
+        url: "https://api.giphy.com/v1/stickers/search?q=" + searchterm + "&api_key=dc6zaTOxFJmzC&limit=1",
         method: "GET",
         success: function(response) {
-            // Below log the response object to the console
-            console.log(response);
-            //Log the data array to the console
-            console.log(response.data);
-            //Log the first gif objectf from the data array
-            console.log(response.data[0]);
-            //Log the images array to the console
-            console.log(response.data[0].images);
-            //Log the orignal image to the console
-            console.log(response.data[0].images.original_still);
-            //Log the url from the orignial gif to the console  
 
-            console.log(response.data[0].images.original_still.url);
             var img_url = response.data[0].images.original.url;
 
             $('body').append('<img src=' + img_url + '>');
 
         },
     });
-$("#search").click(function(){
-var searchterm = $("input").val();
-console.log(searchterm)
-var url= "https://api.giphy.com/v1/stickers/search?q="+searchterm+"&api_key=dc6zaTOxFJmzC&limit=1"
-console.log(url)
-    
+    $("#search").click(function() {
+        console.log(searchterm)
+        var input = $("#search-term").val();
+         console.log(input);
+       getGiphy(input);
+    });
+
+    //function find() {
+    //  var xhttp = new XMLHttpRequest();
+    //    xhttp.open("POST", "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC&limit=10", true);
+    //   xhttp.setRequestHeader("Content-type", "application/json");
+    //  xhttp.send();
+    //    var response = JSON.parse(xhttp.responseText);
+    //}
+
+
+
+
+
+
 });
-
-//function find() {
-  //  var xhttp = new XMLHttpRequest();
-//    xhttp.open("POST", "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC&limit=10", true);
- //   xhttp.setRequestHeader("Content-type", "application/json");
-  //  xhttp.send();
-//    var response = JSON.parse(xhttp.responseText);
-//}
-
-
-
-
-
-
-});
+}
