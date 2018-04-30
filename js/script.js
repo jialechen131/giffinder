34fe1f4aff7b45c4754f29bdcce3baf1f79fa0bc
Jia Le Chen <jialechen321@gmail.com>
2018-04-30 18:18:05 +0000

    it working

--- js/script.js
+++ js/script.js
@@ -1,38 +1,66 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
- function getGiphy(searchterm){
-$(document).ready(function() {
+ //function getGiphy(searchterm){
+//$(document).ready(function() {
+  //  $.ajax({
+    //    url: "https://api.giphy.com/v1/stickers/search?q=" + searchterm + "&api_key=dc6zaTOxFJmzC&limit=1",
+      //  method: "GET",
+        //success: function(response) {
+
+//            var img_url = response.data[0].images.original.url;
+//
+  //          $('body').append('<img src=' + img_url + '>');
+//
+  //      },
+    //});
+    //$("#search").click(function() {
+      //  console.log(searchterm)
+        //var input = $("#search-term").val();
+        // console.log(input);
+       //getGiphy(input);
+    //}); 
+    
+    function giphyURLWithSearchTerm(searchTerm) {
+    var url = "https://api.giphy.com/v1/stickers/search?q="+searchTerm+"&api_key=dc6zaTOxFJmzC";
+    console.log("The url is: " + url);
+    return url;
+}
+
+function displayImage(imageUrl) {
+    $("#result").html("<img src='"+ imageUrl + "'>");
+}
+
+function callGiphyAPIWithSearchTerm(searchTerm) {
+    var apiUrl = giphyURLWithSearchTerm(searchTerm);
+    
     $.ajax({
-        url: "https://api.giphy.com/v1/stickers/search?q=" + searchterm + "&api_key=dc6zaTOxFJmzC&limit=1",
-        method: "GET",
-        success: function(response) {
+      url: apiUrl,
+      method: "GET",
+      success: function(response) {
+          var imageUrl = response.data[0].images.original.url;
+          console.log("The image url is: " + imageUrl);
+          displayImage(imageUrl);
+      },
+    }); 
+}
 
-            var img_url = response.data[0].images.original.url;
-
-            $('body').append('<img src=' + img_url + '>');
-
-        },
-    });
+$(document).ready(function() {
     $("#search").click(function() {
-        console.log(searchterm)
-        var input = $("#search-term").val();
-         console.log(input);
-       getGiphy(input);
+       console.log("Search Button clicked");
+      var search= $("#search-term").val();
+   callGiphyAPIWithSearchTerm(search);
     });
+    
+    
+    
+    
 
-    //function find() {
-    //  var xhttp = new XMLHttpRequest();
-    //    xhttp.open("POST", "https://api.giphy.com/v1/stickers/search?q=dog&api_key=dc6zaTOxFJmzC&limit=10", true);
-    //   xhttp.setRequestHeader("Content-type", "application/json");
-    //  xhttp.send();
-    //    var response = JSON.parse(xhttp.responseText);
-    //}
-
+    
 
 
 
 
 
 });
-}
+//}
